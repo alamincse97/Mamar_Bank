cbd2a9b55b87a37445843d27b5bb89cfc220addf
alamincse97 <alamin.cse97@gmail.com>
2023-08-19 17:40:13 +0600

    added to styl from backend

--- accounts/forms.py
+++ accounts/forms.py
@@ -45,4 +45,21 @@ class UserRegistrationForm(UserCreationForm):
                 account_no = 100000+ our_user.id
             )
         return our_user
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        
+        for field in self.fields:
+            self.fields[field].widget.attrs.update({
+                
+                'class' : (
+                    'appearance-none block w-full bg-gray-200 '
+                    'text-gray-700 border border-gray-200 rounded '
+                    'py-3 px-4 leading-tight focus:outline-none '
+                    'focus:bg-white focus:border-gray-500'
+                ) 
+            })
+
+
+# profile ki ki jinis update korte parbe amader user
     
