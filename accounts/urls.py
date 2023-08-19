65b985e11bc03856bf66755dd07e312ce191e62c
alamincse97 <alamin.cse97@gmail.com>
2023-08-19 18:30:49 +0600

    added to login and logout

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import UserRegistrationView
+from .views import UserRegistrationView, UserLoginView, UserLogoutView
 
 urlpatterns = [
     path('register/', UserRegistrationView.as_view(), name='register'),
-    
+    path('login/', UserRegistrationView.as_view(), name='login'),
+    path('logout/', UserLoginView.as_view(), name='logout'),
 ]
